ca2d28ef83b83f6b397da691968bc135eb861e9e
HerAnthony <heranthony@live.com>
2019-08-14 19:53:17 -0700

    day2 finished starting homework

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// var firstName = 'Tony'
+// var age = 36;
+
+// const firstName = 'Tony';
+// console.log(firstName.length);
+// console.log(firstName);
+
+
+// old way
+// console.log('My name is ' + firstName + ' and my age is ' + age);
+// better way
+// console.log(`My name is ${firstName} and my age is ${age}`);
+
+// alert(`Hello, ${firstName}!`);
+
+// const birthday = '06/28/1983';
+// let fixDate = new Date(birthday);
+// console.log(fixDate);
+
+// let hello = new String('Hello, there.');
+// alert(hello);
+
+// const firstName = 'Tony';
+// const areYouMarried = false;
+// const question = prompt ('Are you married?');
+// if(question === areYouMarried) {
+//     console.log ('Nah, you aint. Too bad!')
+// }
+
+// const topThreeMovies = [`Bill&Ted\'s', 'The Bird\'s', 'Back To The Future`];
+// console.log(topThreeMovies);
+// console.log(`my second top movies is: ${topThreeMovies[1]}`);
